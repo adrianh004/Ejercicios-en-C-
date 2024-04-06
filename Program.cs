fe9c46369ef5e63b89491601144ab53669fe00c0
adrianh004 <99285923+adrianh004@users.noreply.github.com>
2024-04-06 02:05:21 -0400

    Add files via upload
    Este proyecto genera contraseñas seguras de 16 caracteres aleatoriamente.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+
+System.Console.WriteLine("digite la longitud deseada para su contraseña"); //WORD1
+string longitud = Console.ReadLine()!;
+int laar =  Convert.ToInt32(longitud);
+Guid miGuid = Guid.NewGuid(); //esta linea se encarga de generar un identificador unico en el mundo (token) 
+string token = miGuid.ToString().Replace("-", "#"+"*"+"$"+"@").Substring(0, laar);
+System.Console.WriteLine("Su contraseña es: " + token);
